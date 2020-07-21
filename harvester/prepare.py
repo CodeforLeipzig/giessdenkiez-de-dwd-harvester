09c135eeac7581572568f9c90fe659bd4fc01f9e
Joerg Reichert <de.abg.reichert.joerg@googlemail.com>
2020-07-21 22:31:12 +0200

    use Leipzig geo shape

--- harvester/prepare.py
+++ harvester/prepare.py
@@ -2,15 +2,15 @@
 import geopandas
 from shapely.ops import cascaded_union
 
-berlin = geopandas.read_file("./assets/Berlin.shp")
-berlin = berlin.to_crs("epsg:3857")
-berlin_boundary = geopandas.GeoDataFrame(geopandas.GeoSeries(cascaded_union(berlin['geometry'])))
-berlin_boundary = berlin_boundary.rename(columns={0:'geometry'}).set_geometry('geometry')
+leipzig = geopandas.read_file("./assets/ot.shp")
+leipzig = leipzig.to_crs("epsg:3857")
+leipzig_boundary = geopandas.GeoDataFrame(geopandas.GeoSeries(cascaded_union(leipzig['geometry'])))
+leipzig_boundary = leipzig_boundary.rename(columns={0:'geometry'}).set_geometry('geometry')
 
-berlin_buffer = berlin_boundary.buffer(2000)
-berlin_buffer = berlin_buffer.simplify(1000)
+leipzig_buffer = leipzig_boundary.buffer(2000)
+leipzig_buffer = leipzig_buffer.simplify(1000)
 
-berlin_buffer = geopandas.GeoDataFrame(berlin_buffer)
-berlin_buffer = berlin_buffer.rename(columns={0:'geometry'}).set_geometry('geometry')
-berlin_buffer.crs = "epsg:3857"
-berlin_buffer.to_file("./assets/buffer.shp")
+leipzig_buffer = geopandas.GeoDataFrame(leipzig_buffer)
+leipzig_buffer = leipzig_buffer.rename(columns={0:'geometry'}).set_geometry('geometry')
+leipzig_buffer.crs = "epsg:3857"
+leipzig_buffer.to_file("./assets/buffer.shp")
